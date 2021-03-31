e41528923f4faf181fe9b0094c2bcb5fba6b4d2e
Noboru Shiroiwa <shiro25@s-shiro.net>
2021-04-01 00:38:18 +0900

    ユーザー付きブログのダミーデータの用意

--- database/factories/BlogFactory.php
+++ database/factories/BlogFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use App\Models\Blog;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class BlogFactory extends Factory
@@ -22,6 +23,12 @@ class BlogFactory extends Factory
     public function definition()
     {
         return [
+            'user_id' => User::factory(),
+
+            // 'user_id' => function () {
+            //     return User::factory()->create()->id;
+            // },
+
             'title' => $this->faker->realText(20),
             'body' => $this->faker->realText(100),
             'updated_at' => $this->faker->dateTimeBetween('-10days', '0days'),
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Blog;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -16,6 +17,8 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        Blog::factory(30)->create();
+        User::factory(10)->create()->each(function ($user) {
+            Blog::factory(random_int(2, 5))->create(['user_id' => $user]);
+        });
     }
 }
