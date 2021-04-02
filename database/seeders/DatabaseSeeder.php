c715330770631a987b57a21c5f04acd83e1c2896
Noboru Shiroiwa <shiro25@s-shiro.net>
2021-04-02 01:34:47 +0000

    コメントデータの用意

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Blog;
+use App\Models\Comment;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -18,7 +19,9 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
 
         User::factory(10)->create()->each(function ($user) {
-            Blog::factory(random_int(2, 5))->create(['user_id' => $user]);
+            Blog::factory(random_int(2, 5))->create(['user_id' => $user])->each(function ($blog) {
+                Comment::factory(random_int(1,3))->create(['blog_id' => $blog]);
+            });
         });
     }
 }
