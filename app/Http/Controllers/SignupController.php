e1bc54b50f6851c397197979ca9acd8b74ae3f52
Noboru Shiroiwa <shiro25@s-shiro.net>
2021-04-06 07:10:14 +0000

    ユーザー登録後、ログインさせ、マイページにリダイレクト

--- app/Http/Controllers/SignupController.php
+++ app/Http/Controllers/SignupController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use App\Rules\TokyoAddress;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\Rule;
 
 class SignupController extends Controller
@@ -27,22 +29,22 @@ class SignupController extends Controller
         // $address = mb_convert_kana($request->input('address'), 'A');
         // $request->merge(compact('address'));
 
-        $request->validate([
+        $data = $request->validate([
             'name' => ['required', 'string', 'max:20'],
             'email' => ['required', 'email:filter', Rule::unique('users')],
             'password' => ['required', 'string', 'min:8'],
             // 'address' => ['required_if:pref,東京都'],
-            'address' => [new TokyoAddress($request->input('pref'))]
+            // 'address' => [new TokyoAddress($request->input('pref'))]
         ]);
 
-        // ■ 必須系（required, filled, accepted などの存在チェック系）
-        // とにかくチェックが走る
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password']),
+        ]);
 
-        // ■ 一般系
-        // (1) 入力があるときは、チェックは走る（既に他でエラーが出ている場合は除く）
-        // (2) 項目は存在して、値が null の場合、チェックは走る。（但し、'nullable'の指定があれば走らない）
-        // (3) 項目が存在しない場合や空文字列の場合、チェックは走らない。
+        Auth::login($user);
 
-        $request->dd();
+        return redirect('mypage');
     }
 }
