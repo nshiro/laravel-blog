22cd51f304a8f35373289e26f9fd279b5b1c0c48
Noboru Shiroiwa <shiro25@s-shiro.net>
2021-04-06 02:49:30 +0000

    Implicit カスタムルール

--- app/Http/Controllers/SignupController.php
+++ app/Http/Controllers/SignupController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Rules\TokyoAddress;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
 
@@ -20,11 +21,27 @@ class SignupController extends Controller
      */
     public function store(Request $request)
     {
+        // $request->dd();
+
+        //「半角」英数字を「全角」に変換
+        // $address = mb_convert_kana($request->input('address'), 'A');
+        // $request->merge(compact('address'));
+
         $request->validate([
             'name' => ['required', 'string', 'max:20'],
             'email' => ['required', 'email:filter', Rule::unique('users')],
             'password' => ['required', 'string', 'min:8'],
+            // 'address' => ['required_if:pref,東京都'],
+            'address' => [new TokyoAddress($request->input('pref'))]
         ]);
+
+        // ■ 必須系（required, filled, accepted などの存在チェック系）
+        // とにかくチェックが走る
+
+        // ■ 一般系
+        // (1) 入力があるときは、チェックは走る（既に他でエラーが出ている場合は除く）
+        // (2) 項目は存在して、値が null の場合、チェックは走る。（但し、'nullable'の指定があれば走らない）
+        // (3) 項目が存在しない場合や空文字列の場合、チェックは走らない。
 
         $request->dd();
     }
