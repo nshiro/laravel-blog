5d084db8fa44d28ac4338fa9747e243356746237
Noboru Shiroiwa <shiro25@s-shiro.net>
2021-04-07 01:20:59 +0000

    ユーザーログイン

--- app/Http/Controllers/Mypage/UserLoginController.php
+++ app/Http/Controllers/Mypage/UserLoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Mypage;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserLoginController extends Controller
 {
@@ -13,5 +14,26 @@ class UserLoginController extends Controller
     public function index()
     {
         return view('mypage.login');
+    }
+
+    /**
+     * ログイン処理
+     */
+    public function login(Request $request)
+    {
+        $data = $request->validate([
+            'email' => ['required', 'email:filter'],
+            'password' => ['required', 'string'],
+        ]);
+
+        if (Auth::attempt($data)) {
+            $request->session()->regenerate();
+
+            return redirect('mypage');
+        }
+
+        return back()->withErrors([
+            'email' => 'メールアドレスかパスワードが間違っています。',
+        ]);
     }
 }
